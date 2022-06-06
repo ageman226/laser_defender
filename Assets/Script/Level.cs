4903e97119be4bcb3173d303c983b86f22622445
Adriel Maniago <ageman225@gmail.com>
2022-06-06 16:35:57 -0700

    first commits

---
+++ Assets/Script/Level.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 2;
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadMainGame()
+    {
+        FindObjectOfType<GameSession>().ResetGame();
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene("Game Over");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
